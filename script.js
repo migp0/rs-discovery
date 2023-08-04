2c18f4487e6538c4654acf2b2abb88894e1a3f48
migp0 <migpolli@gmail.com>
2023-08-04 12:12:46 -0300

    added error message to unavailable button

--- script.js
+++ script.js
@@ -20,5 +20,18 @@ document.getElementById("username").innerHTML = userName
 
 //subscribe action:::
 function followUser() {
-  alert("You're now following " + username + "!")
+  alert("Let's pretend you're now following " + userName + "!")
+}
+
+//hide ebook msg:::
+function toggleEbookMsg() {
+  //show no ebook msg:::
+  document.getElementById("imsg").innerHTML = "lol, no ebook here."
+
+  var errorMsg = document.getElementById("imsg")
+  if (errorMsg.style.display === "none") {
+    errorMsg.style.display = "block"
+  } else {
+    errorMsg.style.display = "none"
+  }
 }
